5d424698c06041544e10c859680742612ca1233f
inbakumarS <inbakumar1231>
2022-11-21 22:19:44 +0530

    fetching data

--- models/product.js
+++ models/product.js
@@ -1,39 +1,38 @@
 //const products=[];
-const fs=require('fs');
-const path=require('path');
+const fs = require('fs');
+const path = require('path');
 
-module.exports=class Product{
- constructor(t){
-this.title=t;
- }
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
- save(){
-    //products.push(this);//used in models
-    const p=path.join(path.dirname(process.mainModule.filename),'data','products.json');
-    fs.readFile(p, (err, fileContent) => {
-        let products = [];
-        if (!err) {
-          products = JSON.parse(fileContent);
-        }
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-    });
- }
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
- static fetchAll(cb){
-    //return products;
-    const p = path.join(
-        path.dirname(process.mainModule.filename),
-        'data',
-        'products.json'
-      );
-      fs.readFile(p, (err, fileContent) => {
-        if (err) {
-          cb([]);
-        }
-        cb(JSON.parse(fileContent));
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
- }
-}
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
